b9cbc16815c81c544dbbe1555084b876076395b6
Amir Bensekar <aamirbensekar325@gmail.com>
2026-08-16 17:44:11 +0530

    feat: add realtime interaction schema

---
+++ instant.schema.ts
@@ -0,0 +1,27 @@
+import { i } from "@instantdb/react";
+
+const schema = i.schema({
+  entities: {
+    comments: i.entity({
+      imageId: i.string().indexed(),
+      text: i.string(),
+      createdAt: i.date(),
+    }),
+
+    reactions: i.entity({
+      imageId: i.string().indexed(),
+      emoji: i.string(),
+      createdAt: i.date(),
+    }),
+
+    activities: i.entity({
+      imageId: i.string().indexed(),
+      type: i.string(),
+      emoji: i.string().optional(),
+      text: i.string().optional(),
+      createdAt: i.date(),
+    }),
+  },
+});
+
+export default schema;
--- src/lib/instant.ts
+++ src/lib/instant.ts
@@ -1,7 +1,9 @@
 import { init } from "@instantdb/react";
+import schema from "../../instant.schema";
 
 const appId = import.meta.env.VITE_INSTANT_APP_ID;
 
 export const db = init({
   appId,
+  schema,
 });
